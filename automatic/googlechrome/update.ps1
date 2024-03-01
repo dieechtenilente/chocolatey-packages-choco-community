fab2749a78ef68b911c9158ebc08ac37dcbcf9d0
Klaus <dieechtenilente@users.noreply.github.com>
2024-03-01 09:48:08 +0100

    Add files via upload

--- automatic/googlechrome/update.ps1
+++ automatic/googlechrome/update.ps1
@@ -1,37 +1,22 @@
-﻿import-module au
-import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json"
-$paddedUnderVersion = '57.0.2988'
+$FilePath = "./googlechrome.msi"
+$GoogleThumbprint = "2673EA6CC23BEFFDA49AC715B121544098A1284C"
+Invoke-WebRequest -Uri 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $FilePath
 
-function global:au_BeforeUpdate {
-  $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
-  $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+$File = Get-Item -Path $FilePath
+
+$ShellApplication = New-Object -ComObject Shell.Application
+
+$folder = $ShellApplication.Namespace($File.Directory.FullName)
+$file = $folder.ParseName($File.Name)
+
+# Number 24 is the comment field
+$GoogleChromeVersion = ($folder.GetDetailsOf($file, 24) -Split " ")[0]
+
+# Check if certificate is valid and thumbprint matches
+if (
+  (( Get-AuthenticodeSignature .\googlechromestandaloneenterprise64.msi ).Status -eq "Valid") -and 
+  (( Get-AuthenticodeSignature .\googlechromestandaloneenterprise64.msi ).SignerCertificate.Thumbprint -eq $GoogleThumbprint))
+{
+  echo "Executable is valid"
 }
-
-function global:au_SearchReplace {
-  @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.RemoteVersion)'"
-    }
-  }
-}
-
-function global:au_GetLatest {
-  $releasesData = Invoke-RestMethod -UseBasicParsing -Method Get -Uri $releases
-  $version = $releasesData.channels.Stable.version
-  
-  @{
-    URL32 = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
-    URL64 = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-    Version = Get-FixVersion $version -OnlyFixBelowVersion $paddedUnderVersion
-    RemoteVersion = $version
-    PackageName = 'GoogleChrome'
-  }
-}
-
-update -ChecksumFor none
